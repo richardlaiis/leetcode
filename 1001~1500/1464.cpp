d2e55436ff18e07f2e1a3122b147964bcc7c584a
richardlaiis <richardlaiis@proton.me>
2026-07-27 08:57:55 +0800

    backup 20260727

---
+++ 1001~1500/1464.cpp
@@ -0,0 +1,16 @@
+// 1464. maximum-product-of-two-elements-in-an-array
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int a = 0, b = 0;
+        for (int x:nums) {
+            if (x >= a) {
+                b = a;
+                a = x;
+            } else if (x > b) {
+                b = x;
+            }
+        }
+        return (a-1) * (b-1);
+    }
+};
